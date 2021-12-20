05826bed5d60953144fb3d64b2fdcde6fdf4ba52
amaljith c <58605219+amaljithc@users.noreply.github.com>
2021-12-20 14:27:25 +0530

    Add files via upload

---
+++ experiment_6/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.experiment_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b=findViewById(R.id.click);
+        TextView t=findViewById(R.id.textview);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t.setText("hello boy");
+            }
+        });
+    }
+}
